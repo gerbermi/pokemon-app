f635abc569488659870b33bb4bcf141b9f689ae3
Michel Gerber <tux55msge@gmail.com>
2018-12-25 13:02:26 +0100

    Chapitre 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
+
 
 @Component({
 	selector: 'pokemon-app',
-	template: `<h1>Bonjour, {{name}} !</h1><br><h2>Bonne nuit, {{name}}`
+	templateUrl: './app/app.component.html'
 })
-export class AppComponent {name = 'Michel'; }
+export class AppComponent implements OnInit {
+	// 1. La propriété pokemons est un tableau d'objet de type Pokemon
+	pokemons: Pokemon[] = null;
+	ngOnInit() {
+		this.pokemons = POKEMONS;
+		
+	}
+	selectPokemon(pokemon: Pokemon) {
+		console.log('Vous avez sélectionné ' + pokemon.name);
+	}	
+
+}
--- src/dist/app/app.component.js
+++ src/dist/app/app.component.js
@@ -7,14 +7,22 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var mock_pokemons_1 = require("./mock-pokemons");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
-        this.name = 'Michel';
+        // 1. La propriété pokemons est un tableau d'objet de type Pokemon
+        this.pokemons = null;
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.pokemons = mock_pokemons_1.POKEMONS;
+    };
+    AppComponent.prototype.selectPokemon = function (pokemon) {
+        console.log('Vous avez sélectionné ' + pokemon.name);
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'pokemon-app',
-            template: "<h1>Bonjour, {{name}} !</h1><br><h2>Bonne nuit, {{name}}"
+            templateUrl: './app/app.component.html'
         })
     ], AppComponent);
     return AppComponent;
